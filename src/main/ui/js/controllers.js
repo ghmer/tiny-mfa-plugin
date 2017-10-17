82f6fef41ab546db00f8cc9026302f37b9a8c1c3
Mario Enrico Ragucci <mario@whisperedshouts.de>
2017-10-17 15:33:57 +0200

    bugfixing
    csrf token settings on GET requests
    UI enhancements to show error messages, hide divs by default
    try/catch on requests
    
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

--- src/main/ui/js/controllers.js
+++ src/main/ui/js/controllers.js
@@ -11,44 +11,76 @@
 	/** QRCode Controller **/
 	app.controller('QRCodeController', function($scope, $http) {
 		$scope.headline = 'Your QRCode';
+		$scope.qrCodeError   = null;
+		$scope.errorMessage  = null;
+		$scope.qrCodeSuccess = null;
 		
 		function populateQrCode($scope, $http) {
 			$http({
-		        method : "GET",
+		        method  : "GET",
+		        withCredentials: true,
+		        xsrfHeaderName : "X-XSRF-TOKEN",
+		        xsrfCookieName : "CSRF-TOKEN",
 		        url : PluginHelper.getPluginRestUrl('tiny-mfa') + '/generateQrCodeData'
 		    }).then(function mySuccess(response) {
-		    	var el = kjua({text: response.data});
-		        $scope.qrCode = el.src;
-		        $scope.errorMessage = "";
+		    	if(response.data.startsWith("<!DOCTYPE")) {
+		    		// we probably hit a csrf issue
+		    		$scope.errorMessage  = "There was an issue with generating the QRCode. Please proceed with cleaning your browsercookies and restarting the browser before trying again.";
+			        $scope.qrCodeSuccess = null;
+			        $scope.qrCodeError 	 = "Sorry, but the QRCode could not be created";
+		    	} else {
+		    		//generate qrcode 
+		    		var qrCodeObject 	 = kjua({text: response.data});
+			        $scope.qrCode 		 = qrCodeObject.src;
+			        $scope.qrCodeError 	 = null;
+			        $scope.qrCodeSuccess = "success";
+		    	}
 		    }, function myError(response) {
-		        $scope.errorMessage = response.statusText;
+		    	$scope.errorMessage  = "There was an issue with generating the QRCode. Please proceed with cleaning your browsercookies and restarting the browser before trying again.";
+			    $scope.qrCodeSuccess = null;
+		        $scope.qrCodeError   = "Sorry, but the QRCode could not be created";
 		    });
 		};
+		try {
+			populateQrCode($scope, $http);
+		}catch(error) {
+			$scope.errorMessage  = error.message;
+			$scope.qrCodeError   = "Sorry, but the QRCode could not be created";
+	        $scope.qrCodeSuccess = null;
+		}
 		
-		populateQrCode($scope, $http);
 	});
 	
 	/** ValidateController Controller **/
 	app.controller('ValidateController', function($scope, $http) {
 		$scope.headline = 'Validate Your QRCode';
+		$scope.validationError 	 = null;
+		$scope.validationSuccess = null;
 		
-		$scope.validateToken = function(tokenValue){
-			$http({
-		        method : "GET",
-		        url : PluginHelper.getPluginRestUrl('tiny-mfa') + '/validateToken/' + PluginHelper.getCurrentUsername() + '/' + tokenValue
-		    }).then(function mySuccess(response) {
-		    	if(response.data == "true") {
-		    		$scope.validationSuccess = "Token validation successful";
-			    	$scope.validationError = null;
-		    	} else {
-		    		$scope.validationError = "Token validation failed - " + response.data;
+		try {
+			$scope.validateToken = function(tokenValue){
+				$http({
+			        method  : "GET",
+			        withCredentials: true,
+			        xsrfHeaderName : "X-XSRF-TOKEN",
+			        xsrfCookieName : "CSRF-TOKEN",
+			        url : PluginHelper.getPluginRestUrl('tiny-mfa') + '/validateToken/' + PluginHelper.getCurrentUsername() + '/' + tokenValue
+			    }).then(function mySuccess(response) {
+			    	if(response.data == "true") {
+			    		$scope.validationSuccess = "Token validation successful";
+				    	$scope.validationError   = null;
+			    	} else {
+			    		$scope.validationError   = "Token validation failed - " + response.data;
+				    	$scope.validationSuccess = null;
+			    	}
+			    	
+			    }, function myError(response) {
+			    	$scope.validationError   = "Token validation failed - " + response.data;
 			    	$scope.validationSuccess = null;
-		    	}
-		    	
-		    }, function myError(response) {
-		    	$scope.validationError = "Token validation failed - " + response.data;
-		    	$scope.validationSuccess = null;
-		    });
-		};
+			    });
+			};
+		}catch(error) {
+			$scope.validationError = error.message;
+		}
 	});
 }());
